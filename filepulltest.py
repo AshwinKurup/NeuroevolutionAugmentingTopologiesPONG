84933cd6c405e1dc8c6f9542634ff68de7c14859
AshwinKurup <akurup120@gmail.com>
2021-09-27 19:19:42 +0800

    Delete filepulltest.py

--- filepulltest.py
+++
@@ -1,4 +0,0 @@
-# try to pull the test file off the github page and run the python code here
-# there's a github API so can just use that LOL
-# that's the first test that's all that needs to be done here
-import
